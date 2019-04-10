d7c3b73ac50a9d4011fd2efb4159ab94d9496aa3
Max Uroda <ummmaxxx@gmail.com>
2019-04-10 18:50:34 +0300

    Added buttons for CV, stackoverflow-stories, stackoverflow-cv, and stackexchange.

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Wrapper from './Wrapper';
 import SocialLink from './SocialLink';
 
-function Social({ magentou, linkedin, email, skype, calendy, commercehero, twitter, github, dockerhub, pagespeed, lighthouse }) {
+function Social({ magentou, linkedin, cv, email, skype, calendy, commercehero, stackoverflowstory, stackoverflowcv, stackexchange, twitter, github, dockerhub, pagespeed, lighthouse }) {
   return (
     <Wrapper>
       {magentou && (
@@ -27,6 +27,18 @@ function Social({ magentou, linkedin, email, skype, calendy, commercehero, twitt
               <circle cx="4" cy="4" r="2"></circle>
             </svg>
             LinkedIn
+          </SocialLink>
+      )}
+      {cv && (
+          <SocialLink href={cv} target="_blank" rel="noopener">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 className="feather feather-download-cloud">
+              <polyline points="8 17 12 21 16 17"></polyline>
+              <line x1="12" y1="12" x2="12" y2="21"></line>
+              <path d="M20.88 18.09A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.29"></path>
+            </svg>
+            Request a CV
           </SocialLink>
       )}
       {email && (
@@ -74,6 +86,48 @@ function Social({ magentou, linkedin, email, skype, calendy, commercehero, twitt
                   d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
             </svg>
             Commerce Hero
+          </SocialLink>
+      )}
+      {stackoverflowstory && (
+          <SocialLink href={stackoverflowstory} target="_blank" rel="noopener">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 className="feather feather-list">
+              <line x1="8" y1="6" x2="21" y2="6"></line>
+              <line x1="8" y1="12" x2="21" y2="12"></line>
+              <line x1="8" y1="18" x2="21" y2="18"></line>
+              <line x1="3" y1="6" x2="3" y2="6"></line>
+              <line x1="3" y1="12" x2="3" y2="12"></line>
+              <line x1="3" y1="18" x2="3" y2="18"></line>
+            </svg>
+            StackOverflow Story
+          </SocialLink>
+      )}
+      {stackoverflowcv && (
+          <SocialLink href={stackoverflowcv} target="_blank" rel="noopener">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 className="feather feather-file-text">
+              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+              <polyline points="14 2 14 8 20 8"></polyline>
+              <line x1="16" y1="13" x2="8" y2="13"></line>
+              <line x1="16" y1="17" x2="8" y2="17"></line>
+              <polyline points="10 9 9 9 8 9"></polyline>
+            </svg>
+            StackOverflow CV
+          </SocialLink>
+      )}
+      {stackexchange && (
+          <SocialLink href={stackexchange} target="_blank" rel="noopener">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 className="feather feather-users">
+              <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+              <circle cx="9" cy="7" r="4"></circle>
+              <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+              <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+            </svg>
+            Stackexchange
           </SocialLink>
       )}
       {twitter && (
