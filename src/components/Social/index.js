5417d17fc03fef6a758a78558192a9d0aae8752c
Max Uroda <ummmaxxx@gmail.com>
2019-05-05 17:25:03 +0300

    Updated personal info, added link to Mage2.click website.

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Wrapper from './Wrapper';
 import SocialLink from './SocialLink';
 
-function Social({ magentou, linkedin, cv, email, skype, calendy, commercehero, stackoverflowstory, stackoverflowcv, stackexchange, twitter, github, dockerhub, pagespeed, lighthouse }) {
+function Social({ magentou, linkedin, cv, email, skype, calendy, commercehero, stackoverflowstory, stackoverflowcv, stackexchange, twitter, github, dockerhub, pagespeed, lighthouse, mage2click}) {
   return (
     <Wrapper>
       {magentou && (
@@ -186,6 +186,19 @@ function Social({ magentou, linkedin, cv, email, skype, calendy, commercehero, s
               <line x1="10" y1="14" x2="21" y2="3"></line>
             </svg>
             Lighthouse Test
+          </SocialLink>
+      )}
+      {mage2click && (
+          <SocialLink href={mage2click} target="_blank" rel="noopener">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                 className="feather feather-user-plus">
+              <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+              <circle cx="8.5" cy="7" r="4"></circle>
+              <line x1="20" y1="8" x2="20" y2="14"></line>
+              <line x1="23" y1="11" x2="17" y2="11"></line>
+            </svg>
+            Mage2.click
           </SocialLink>
       )}
     </Wrapper>
