5d2cac8cec7d6c659c533fef6c211640a6bae412
Max Uroda <ummmaxxx@gmail.com>
2022-07-02 11:28:48 +0200

    Updated info, removed outdated links

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Max Uroda Certified Magento Developer', // Required
   author: 'Max Uroda', // Required
   description: "I am a Certified Magento Developer (M1CD+ / M2CAD / M2CFD) with over 9 years of experience in Web Development, Software Engineer, Independent Contractor and member of Mage2.click - Magento Users Group from Khmelnytskyi, Ukraine. I have a Master's degree in Computer Science.",
-  detailed_description: 'I am implementing and supporting highly loaded B2C and B2B e-commerce projects based on Magento and Magento 2, leading a team of developers, providing performance optimizations and the right solution for any type of issues. I have a deep understanding and experience working with the following technology stack: PHP, MySQL, JavaScript, jQuery, AJAX, REST, HTML, CSS, Git, Linux, Nginx, Apache, Redis, Varnish, Elasticsearch, AWS, New Relic, Docker. Besides that, I always keep learning something new like GatsbyJs and PWA which I have used to create this website.',
+  detailed_description: 'I am implementing and supporting highly loaded B2C and B2B e-commerce projects based on Magento and Magento 2, leading a team of developers, providing performance optimizations and the right solution for any type of issues. I have a deep understanding and experience working with the following technology stack: PHP, MySQL, JavaScript, jQuery, AJAX, REST, HTML, CSS, Git, Linux, Nginx, Apache, Redis, Varnish, Elasticsearch, AWS, New Relic, Docker, Blackfire.io. Besides that, I always keep learning something new like GatsbyJs and PWA which I have used to create this website.',
   primaryColor: '#f46f25', // Required
   showHeaderImages: true,
   magentoUtitle: 'M1 Developer Plus',
@@ -12,19 +12,15 @@ module.exports = {
   social: {
     magentou: 'https://www.credly.com/users/umax/badges',
     linkedin: 'https://www.linkedin.com/in/max-uroda/',
-    cv: 'https://drive.google.com/file/d/10feizmxpyFIbUNGy1L4kRV28H44urIkN/view?usp=sharing',
+    cv: 'https://drive.google.com/file/d/1CthNtkF5cEJgAQTwVSdgcirvIwsX0tju/view?usp=sharing',
     email: 'mailto:contact@maxuroda.pro',
-    skype: 'https://join.skype.com/invite/obgKpBOEJZed',
-    calendy: "https://calendly.com/max-uroda",
+    calendy: "https://calendly.com/ummmaxxx/30min",
     commercehero: 'https://commercehero.io/u_maxx',
-    stackoverflowstory: 'https://stackoverflow.com/story/max-uroda',
-    stackoverflowcv: 'https://stackoverflow.com/cv/max-uroda',
     stackexchange: 'https://stackexchange.com/users/2047361/max-uroda?tab=accounts',
     twitter: 'https://twitter.com/u_maxx',
     github: 'https://github.com/u-maxx',
     dockerhub: 'https://hub.docker.com/u/umaxx',
     pagespeed: "https://developers.google.com/speed/pagespeed/insights/?url=https%3A%2F%2Fmaxuroda.pro%2F",
-    lighthouse: "https://www.webpagetest.org/lighthouse?url=https://maxuroda.pro",
     mage2click: "https://mage2.click"
   },
   pathPrefix: '/devblog',
