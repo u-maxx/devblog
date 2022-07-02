8b3e22764ecd7da814724b897584fc0a70c9d0a0
Max Uroda <ummmaxxx@gmail.com>
2022-07-02 10:32:40 +0200

    Updated info

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 module.exports = {
   title: 'Max Uroda Certified Magento Developer', // Required
   author: 'Max Uroda', // Required
-  description: "I am a Certified Magento Developer (M1CD+ / M2CAD / M2CFD) with over 9 years of experience in Web Development, Software Engineer, Independent Contractor and member of Mage2.click - Magento Users Group from Khmelnytskyi, Ukraine. I have a Master's degree in Computer Science. Currently working at SwiftOtter, Inc.",
+  description: "I am a Certified Magento Developer (M1CD+ / M2CAD / M2CFD) with over 9 years of experience in Web Development, Software Engineer, Independent Contractor and member of Mage2.click - Magento Users Group from Khmelnytskyi, Ukraine. I have a Master's degree in Computer Science.",
   detailed_description: 'I am implementing and supporting highly loaded B2C and B2B e-commerce projects based on Magento and Magento 2, leading a team of developers, providing performance optimizations and the right solution for any type of issues. I have a deep understanding and experience working with the following technology stack: PHP, MySQL, JavaScript, jQuery, AJAX, REST, HTML, CSS, Git, Linux, Nginx, Apache, Redis, Varnish, Elasticsearch, AWS, New Relic, Docker. Besides that, I always keep learning something new like GatsbyJs and PWA which I have used to create this website.',
   primaryColor: '#f46f25', // Required
   showHeaderImages: true,
   magentoUtitle: 'M1 Developer Plus',
-  magento2Utitle: 'I am actively preparing for M2 certifications',
+  magento2Utitle: 'Adobe Certified Professional — Magento Commerce Developer, Adobe Certified Expert - Magento Commerce Front-End Developer',
   showShareButtons: true,
   postsPerPage: 1, // Required
   social: {
-    magentou: 'https://u.magento.com/certification/directory/dev/1122780/',
+    magentou: 'https://www.credly.com/users/umax/badges',
     linkedin: 'https://www.linkedin.com/in/max-uroda/',
     cv: 'https://drive.google.com/file/d/10feizmxpyFIbUNGy1L4kRV28H44urIkN/view?usp=sharing',
     email: 'mailto:contact@maxuroda.pro',
