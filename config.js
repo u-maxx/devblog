41e9c62c7484910caef231e32b624be7f7ede600
Max Uroda <ummmaxxx@gmail.com>
2023-11-12 09:34:06 +0100

    Update info in config.js

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: 'Max Uroda Certified Magento Developer', // Required
+  title: 'Max Uroda Certified Adobe Commerce and Magento Developer', // Required
   author: 'Max Uroda', // Required
-  description: "I am a Certified Magento Developer (M1CD+ / M2CAD / M2CFD) with over 9 years of experience in Web Development, Software Engineer, Independent Contractor and member of Mage2.click - Magento Users Group from Khmelnytskyi, Ukraine. I have a Master's degree in Computer Science.",
+  description: "I am a Certified Certified Adobe Commerce and Magento Developer (M1CD+ / M2CAD / M2CFD) with over 12 years of experience in Web Development, Software Engineer, Independent Contractor and member of Mage2.click - Magento Users Group from Khmelnytskyi, Ukraine. I have a Master's degree in Computer Science.",
   detailed_description: 'I am implementing and supporting highly loaded B2C and B2B e-commerce projects based on Magento and Magento 2, leading a team of developers, providing performance optimizations and the right solution for any type of issues. I have a deep understanding and experience working with the following technology stack: PHP, MySQL, JavaScript, jQuery, AJAX, REST, HTML, CSS, Git, Linux, Nginx, Apache, Redis, Varnish, Elasticsearch, AWS, New Relic, Docker, Blackfire.io. Besides that, I always keep learning something new like GatsbyJs and PWA which I have used to create this website.',
   primaryColor: '#f46f25', // Required
   showHeaderImages: true,
   magentoUtitle: 'M1 Developer Plus',
-  magento2Utitle: 'Adobe Certified Professional — Magento Commerce Developer, Adobe Certified Expert - Magento Commerce Front-End Developer',
+  magento2Utitle: 'Adobe Certified Professional — Certified Adobe Commerce and Magento Commerce Developer, Adobe Certified Expert - Magento Commerce Front-End Developer',
   showShareButtons: true,
   postsPerPage: 1, // Required
   social: {
     magentou: 'https://www.credly.com/users/umax/badges',
     linkedin: 'https://www.linkedin.com/in/max-uroda/',
-    cv: 'https://drive.google.com/file/d/1CthNtkF5cEJgAQTwVSdgcirvIwsX0tju/view?usp=sharing',
-    email: 'mailto:contact@maxuroda.pro',
-    calendy: "https://calendly.com/ummmaxxx/30min",
-    commercehero: 'https://commercehero.io/u_maxx',
+    cv: 'https://drive.google.com/file/d/1gzulEzh97M051ICW7L8AtLKLp9IXl0Q1/view?usp=sharing',
+    email: 'mailto:maxuroda@zohomail.eu',
+    calendy: "https://calendly.com/max-uroda/15min",
+    gravatar: 'https://gravatar.com/maxuroda',
     stackexchange: 'https://stackexchange.com/users/2047361/max-uroda?tab=accounts',
     twitter: 'https://twitter.com/u_maxx',
     github: 'https://github.com/u-maxx',
     dockerhub: 'https://hub.docker.com/u/umaxx',
-    pagespeed: "https://developers.google.com/speed/pagespeed/insights/?url=https%3A%2F%2Fmaxuroda.pro%2F",
+    blog: "https://maxuroda.wordpress.com/",
     mage2click: "https://mage2.click"
   },
   pathPrefix: '/devblog',
